aee93dac7cd14f114204f908c580a2faea2afebd
heru <113361846+mocheru@users.noreply.github.com>
2023-05-26 23:17:57 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '5c019b616cbe23329f79d45f8745bd9c',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
